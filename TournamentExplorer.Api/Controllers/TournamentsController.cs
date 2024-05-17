4a64dd406ce39dbca9f7385e17a92435bd57d917
John Källström <johnkaellstrom@gmail.com>
2024-05-17 14:06:39 +0200

    Add project files.

---
+++ TournamentExplorer.Api/Controllers/TournamentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TournamentExplorer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TournamentsController : ControllerBase
+    {
+        public ActionResult Ping() => Ok("Hello World!");
+    }
+}
