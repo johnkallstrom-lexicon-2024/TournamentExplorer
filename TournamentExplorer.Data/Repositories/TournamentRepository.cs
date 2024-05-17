6c0e6cff1b29bffefc3d8273affe4992d37e5371
John Källström <johnkaellstrom@gmail.com>
2024-05-17 16:40:08 +0200

    Repository implementation

---
+++ TournamentExplorer.Data/Repositories/TournamentRepository.cs
@@ -0,0 +1,38 @@
+﻿using TournamentExplorer.Core.Contracts;
+using TournamentExplorer.Core.Entities;
+
+namespace TournamentExplorer.Data.Repositories
+{
+    public class TournamentRepository : ITournamentRepository
+    {
+        public Task<bool> AnyAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tournament> CreateAsync(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Tournament>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tournament> GetAsync(int id, bool includeGames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
