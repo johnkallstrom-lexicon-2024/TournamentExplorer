8991ef6fc6196d0ebae427a107ec40744864b814
John Källström <johnkaellstrom@gmail.com>
2024-05-19 17:06:39 +0200

    Update tournament

--- TournamentExplorer.Core/Entities/Tournament.cs
+++ TournamentExplorer.Core/Entities/Tournament.cs
@@ -8,9 +8,9 @@ namespace TournamentExplorer.Core.Entities
         [Key]
         public int Id { get; set; }
         public string Title { get; set; } = default!;
-        public string Description { get; set; } = default!;
-        public string Location { get; set; } = default!;
         public DateTime StartDate { get; set; }
+        public string City { get; set; } = default!;
+        public string Country { get; set; } = default!;
         public TournamentType Type { get; set; }
 
         public ICollection<Game> Games { get; set; } = new List<Game>();
---
+++ TournamentExplorer.Data/Migrations/20240519145920_AddCityAndCountryToTournament.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TournamentExplorer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCityAndCountryToTournament : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "Tournament",
+                newName: "Country");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Tournament",
+                newName: "City");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Country",
+                table: "Tournament",
+                newName: "Location");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Tournament",
+                newName: "Description");
+        }
+    }
+}
