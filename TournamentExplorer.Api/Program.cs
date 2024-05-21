b34366dcecd0a75a48b921101f0eb27a124549b1
John Källström <johnkaellstrom@gmail.com>
2024-05-21 15:45:31 +0200

    Add support for requesting data in xml format

--- TournamentExplorer.Api/Program.cs
+++ TournamentExplorer.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(config =>
 {
     config.ReturnHttpNotAcceptable = true;
-});
+}).AddXmlDataContractSerializerFormatters();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
