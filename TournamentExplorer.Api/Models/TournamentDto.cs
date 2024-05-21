162a8f1520b378df6eb0e83e1bbcb9b0d1dda6fb
John Källström <johnkaellstrom@gmail.com>
2024-05-21 15:25:58 +0200

    Convert enum to string on dtos

--- TournamentExplorer.Api/Models/TournamentDto.cs
+++ TournamentExplorer.Api/Models/TournamentDto.cs
@@ -1,4 +1,5 @@
-﻿using TournamentExplorer.Core.Enums;
+﻿using System.Text.Json.Serialization;
+using TournamentExplorer.Core.Enums;
 
 namespace TournamentExplorer.Api.Models
 {
@@ -7,8 +8,10 @@ namespace TournamentExplorer.Api.Models
         public int Id { get; set; }
         public string Title { get; set; } = default!;
         public DateTime StartDate { get; set; }
+        public DateTime EndDate => StartDate.AddMonths(3);
         public string City { get; set; } = default!;
         public string Country { get; set; } = default!;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public TournamentType Type { get; set; }
 
         public IEnumerable<GameWithoutRelationsDto> Games { get; set; } = new List<GameWithoutRelationsDto>();
